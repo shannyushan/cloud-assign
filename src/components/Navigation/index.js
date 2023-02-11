9deca8bc258ee94a539a3e157f78c75b09605143
shannyushan <nyushan.darshan@gmail.com>
2023-02-11 14:55:29 +0545

    basic navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, {Link, useState} from 'react'
+import { CartIcon } from '../../globals/icons/icons'
+
+
+const NAVLINKS  = [
+  {
+    "name": "Home",
+    "link": "/"
+  },
+  {
+    "name": "Shop",
+    "link": "/shop"
+  },
+  {
+    "name": "About",
+    "link": "/about"
+  },
+  {
+    "name": "Contact",
+    "link": "/contact"
+  },
+  {
+    "name": "FAQ",
+    "link": "/faq"
+  }
+]
+
 
 const Navigation = () => {
+  const [user, setUser] = useState({})
+  const [cart, setCart] = useState(false)
+  const [profile, setProfile] = useState(false)
+  const [search, setSearch] = useState(false)
+
+
+    function handleCart(){
+      return
+    }
+    function handleProfile(){
+      return
+    }
+
   return (
-    <div>Navigation</div>
+    <div className='d-flex backgroundcolor class'>
+      <div className=''>
+        <span className='brand'>SimplexDecor</span>
+      </div>
+
+
+      <ul className='flex-row-item'>
+        {NAVLINKS.map(navitem =>{
+          return (
+            <Link to= {navitem.link} className="nav-item">{navitem.name}</Link>
+          )
+        })}
+      </ul>
+      <ul className='sideoptions '>
+        <div className='flex-box' onClick={e => handleCart()}>
+            {CartIcon(16, "black")}
+          </div>
+        <div className='flex-box' onClick={e => handleProfile()}>
+          {ProfileIcon(16, "black")}
+        </div>
+        <div className='flex-box' onClick={e => setSearch(true)}>
+          {SearchIcon(16, "black")}
+        </div>
+      </ul>
+    </div>
   )
 }
+
+
+
 
 export default Navigation
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ["./src//*.{html,js}"],
+    theme: {
+      extend: {
+
+         boxShadow: {
+        card: '2px 2px 100px 40px rgba(0, 0, 0, 0.05)',
+      },
+         colors: {
+        primary: { //ultramarine
+          DEFAULT: '#083f89',
+          'w-50': '#f8f6f7',
+          'o-100': '#ff9d01',
+          'b-200' : '#0f0506'
+
+        },
+      },
+      },
+    },
+    plugins: [],
+  }
